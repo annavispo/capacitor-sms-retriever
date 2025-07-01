76031c474aabdb8dc9dbe2b016da8965e5d59275
Annarosa Vispo <annarosa.vispo@etnadg.com>
2025-07-01 10:59:16 +0200

    Aggiunta dist/ generata per Capacitor 7

---
+++ dist/plugin.cjs.js
@@ -0,0 +1,30 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var core = require('@capacitor/core');
+
+const CapacitorSmsRetriever = core.registerPlugin('CapacitorSmsRetriever', {
+    web: () => Promise.resolve().then(function () { return web; }).then(m => new m.CapacitorSmsRetrieverWeb()),
+});
+
+class CapacitorSmsRetrieverWeb extends core.WebPlugin {
+    async startListening() {
+        console.warn('Capacitor SMS Retriever not available on web');
+        return Promise.resolve({ body: 'Capacitor SMS Retriever not available on web' });
+    }
+    async stopListening() {
+        return Promise.reject(new Error('Capacitor SMS Retriever not available on web'));
+    }
+    async present() {
+        return Promise.reject(new Error('Capacitor SMS Retriever not available on web'));
+    }
+}
+
+var web = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    CapacitorSmsRetrieverWeb: CapacitorSmsRetrieverWeb
+});
+
+exports.CapacitorSmsRetriever = CapacitorSmsRetriever;
+//# sourceMappingURL=plugin.cjs.js.map
